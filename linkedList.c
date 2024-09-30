c843f1a8b05921339e6cababdd80755cf520d981
diwashpandey1 <diwashpandey999@gmail.com>
2024-09-30 16:22:43 +0530

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -7,7 +7,6 @@ struct node {
 };
 
 struct node *head = NULL;
-struct node *current = NULL;
 
 void printList() {
     struct node *p = head;
@@ -20,60 +19,139 @@ void printList() {
 }
 
 void insertAtBegin(int data) {
-    struct node *lk = (struct node*) malloc(sizeof(struct node));
-    if (lk == NULL) {
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
+    if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return;
     }
-    lk->data = data;
-    lk->next = head;
-    head = lk;
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
 }
 
 void insertAtMiddle(int data) {
-    printf("Working!");
-}
-
-void insertAtEnd(int data) {
-    struct node *lk = (struct node*) malloc(sizeof(struct node));
-    struct node *last = head;
-    if (lk == NULL) {
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
+    if (newNode == NULL) {
         printf("Memory allocation failed.\n");
         return;
     }
-    lk->data = data;
-    lk->next = NULL;
+    newNode->data = data;
+
+    // If list is empty or we insert at the beginning
+    if (head == NULL || head->data >= data) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+
+    // Locate the node after which the new node is to be inserted
+    struct node *current = head;
+    while (current->next != NULL && current->next->data < data) {
+        current = current->next;
+    }
+    
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+void insertAtEnd(int data) {
+    struct node *newNode = (struct node*) malloc(sizeof(struct node));
+    struct node *last = head;
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
     
     if (head == NULL) {
-        head = lk;
+        head = newNode;
         return;
     }
 
     while (last->next != NULL) {
         last = last->next;
     }
-    last->next = lk;
-    
+    last->next = newNode;
 }
 
 void deleteAtBegin() {
-    printf("Working!");
+    if (head == NULL) {
+        printf("List is empty, nothing to delete.\n");
+        return;
+    }
+    struct node *temp = head;
+    head = head->next;
+    free(temp);
+    printf("Deleted the first element.\n");
 }
 
 void deleteAtMiddle(int data) {
-    printf("Working!");
+    if (head == NULL) {
+        printf("List is empty, nothing to delete.\n");
+        return;
+    }
+
+    struct node *current = head;
+    struct node *prev = NULL;
+
+    while (current != NULL && current->data != data) {
+        prev = current;
+        current = current->next;
+    }
+
+    // If the data was not found
+    if (current == NULL) {
+        printf("Element %d not found in the list.\n", data);
+        return;
+    }
+
+    // Unlink the node from linked list
+    if (prev == NULL) {
+        head = current->next; // The node to delete is the head
+    } else {
+        prev->next = current->next;
+    }
+    
+    free(current);
+    printf("Deleted the element %d.\n", data);
 }
 
 void deleteAtEnd() {
-    printf("Working!");
+    if (head == NULL) {
+        printf("List is empty, nothing to delete.\n");
+        return;
+    }
+    
+    struct node *current = head;
+    struct node *prev = NULL;
+
+    // If the list contains only one node
+    if (current->next == NULL) {
+        free(current);
+        head = NULL;
+        printf("Deleted the last element.\n");
+        return;
+    }
+
+    // Traverse to the last node
+    while (current->next != NULL) {
+        prev = current;
+        current = current->next;
+    }
+
+    // Unlink the last node
+    prev->next = NULL;
+    free(current);
+    printf("Deleted the last element.\n");
 }
 
-int searchData(int Data) {
+int searchData(int data) {
     struct node *p = head;
     while (p != NULL) {
-        if(p->data == Data){
-        	return 1;
-		}
+        if (p->data == data) {
+            return 1;
+        }
         p = p->next;
     }
     return 0;
@@ -81,7 +159,7 @@ int searchData(int Data) {
 
 int main() {
     int ch;
-    int Data;
+    int data;
     int k = 0;
     do {
         printf("\nLinked List Operations: ");
@@ -94,26 +172,26 @@ int main() {
         switch (ch) {
             case 1:
                 printf("\nEnter the value to insert: ");
-                scanf("%d", &Data);
-                insertAtBegin(Data);
+                scanf("%d", &data);
+                insertAtBegin(data);
                 break;
             case 2:
                 printf("\nEnter the value to insert: ");
-                scanf("%d", &Data);
-                insertAtMiddle(Data);
+                scanf("%d", &data);
+                insertAtMiddle(data);
                 break;
             case 3:
                 printf("\nEnter the value to insert: ");
-                scanf("%d", &Data);
-                insertAtEnd(Data);
+                scanf("%d", &data);
+                insertAtEnd(data);
                 break;
             case 4:
                 deleteAtBegin();
                 break;
             case 5:
                 printf("\nEnter the value to delete: ");
-                scanf("%d", &Data);
-                deleteAtMiddle(Data);
+                scanf("%d", &data);
+                deleteAtMiddle(data);
                 break;
             case 6:
                 deleteAtEnd();
@@ -123,13 +201,13 @@ int main() {
                 break;
             case 8:
                 printf("\nEnter the value to search: ");
-                scanf("%d", &Data);
-                k = searchData(Data);
-                if(k == 1){
-                	printf("\nElement found!\n");
-				} else {
-					printf("\nElement Not found!\n");
-				}
+                scanf("%d", &data);
+                k = searchData(data);
+                if (k == 1) {
+                    printf("\nElement found!\n");
+                } else {
+                    printf("\nElement Not found!\n");
+                }
                 break;
             case 9:
                 printf("\nExiting......\n");
